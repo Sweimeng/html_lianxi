95183ea71905aca6043e49b31a2f082ad16a3bf8
Sweimeng <404953054@163.com>
2024-05-13 20:17:44 +0800

    Add files via upload

---
+++ test_html.py
@@ -0,0 +1,10 @@
+import pymysql
+
+# conn = pymysql.connect(host="127.0.0.1", port=3306, user='root', passwd="root123" , database="unicom",charset='utf8')
+# cursor = conn.cursor()
+#
+# cursor.execute("insert into admin(username,password,mobile) values('def','def123','13888888888')")
+# conn.commit()
+# cursor.close()
+# conn.close()
+
