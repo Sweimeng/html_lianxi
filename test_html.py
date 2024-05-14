e25f0cbfe8dc59597d7701cb42894bc3909060f3
Sweimeng <404953054@163.com>
2024-05-14 19:58:36 +0800

    Update test_html.py

--- test_html.py
+++ test_html.py
@@ -1,4 +1,13 @@
 import pymysql
+from flask import Flask
+
+app=Flask(__name__)
+@app.route("/show/info")
+def index():
+  return "1.html"
+
+if __name__=='__main__':
+  app.run()
 
 # conn = pymysql.connect(host="127.0.0.1", port=3306, user='root', passwd="root123" , database="unicom",charset='utf8')
 # cursor = conn.cursor()
